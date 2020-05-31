1fa58426e7dfca67d9ca9a8305d47bb50babbf6a
EOussama <oussama_gtacj@live.fr>
2020-05-31 19:11:11 +0100

    Added ground set up

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log('Arena Survival');
+import {
+  Engine,
+  Actor,
+  EngineOptions
+} from "excalibur";
+
+const options: EngineOptions = {};
+const game = new Engine(options);
+
+game.start();
